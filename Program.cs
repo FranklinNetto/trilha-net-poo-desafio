8c1bbacb321a9a82daf1adc579bbc1fe99bdc529
FranklinNetto <fknetto@gmail.com>
2024-04-08 20:57:18 -0300

    Implementação de classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n***Smartphone Nokia***");
+
+Smartphone nokia = new Nokia("99 88888-7777", "G11 Plus", "111111111", 128 );
+nokia.Ligar();
+nokia.InstalarAplicativo("FaceBook");
+
+
+Console.WriteLine("\n***Smartphone - Iphone***");
+
+Smartphone iphone = new Iphone("88 99999-6666", "15 Pro Max", "222222222", 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tinder");
